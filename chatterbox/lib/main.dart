1883fdfd824b22ce7adba3f36ecc847efe3f2260
joshuadlima <joshd11723@gmail.com>
2025-09-27 12:26:10 +0530

    directory cleanup

--- chatterbox/lib/main.dart
+++ chatterbox/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Counter App'),
+      home: const MyHomePage(title: 'ChatterBox'),
     );
   }
 }
